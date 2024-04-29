6696c3dd7b536b1653e5e2a5bf29ac456ff4aecc
issrainnourneji <issrainnour.neji@esprit.tn>
2024-04-29 14:01:19 +0200

    First commit

---
+++ src/main/java/com/muntu/muntuhabitat/MuntuHabitatApplication.java
@@ -0,0 +1,13 @@
+package com.muntu.muntuhabitat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuntuHabitatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuntuHabitatApplication.class, args);
+    }
+
+}
